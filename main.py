3f0e3d5fe489561900a004dc417afed9aed618db
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-01 13:55:02 -0300

    new: add count elements when insert

--- main.py
+++ main.py
@@ -7,20 +7,23 @@ class PairKeyValue:
 class HashTable:
     def __init__(self):
         self.__capacity = 5
+        self.count = 0
         self.__table = [[] for _ in range(self.__capacity)]
 
     def hashCode(self, key):
         strKey = str(key)
         return hash(strKey) % self.__capacity
 
+    def __len__(self):
+        return self.count
+
     def __setitem__(self, key, value):
         index = self.hashCode(key)
         self.__table[index].append(PairKeyValue(key, value))
-        
+        self.count += 1
 
 
 my_hash = HashTable()
 my_hash['nome'] = 'Luiz'
-
 
 
