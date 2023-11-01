d204a994e2f5ac95cd50251dd4cb6eb1e2e9ae18
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-01 15:55:26 -0300

    new: create __delitem__ method

--- main.py
+++ main.py
@@ -33,10 +33,23 @@ class HashTable:
         self.__table[index].append(new_element)
         self.count += 1
 
+    def __delitem__(self, key):
+        index = self.hashCode(key)
+        for element in self.__table[index]:
+            if element.key == key:
+                self.__table[index].remove(element)
+                self.count -= 1
+                return element.value
+        
+        raise KeyError(f'{key}')
 
 phones = HashTable()
 phones['Steve'] = '9999-1111'
 phones['Maria'] = '9999-2222'
 phones['Alex'] = '9999-3333'
 phones['Alex'] = '1111-3333'
+del phones['Alex']
+del phones['Maria']
+del phones['Steve']
+del phones['Alex']
 print()
