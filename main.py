e5bc99a41bd824e612f98344459ff33d40f24a4e
Luiz Guilherme <luizgmelo64@gmail.com>
2023-11-01 16:02:44 -0300

    fix: self.count private

--- main.py
+++ main.py
@@ -13,15 +13,16 @@ class PairKeyValue:
 class HashTable:
     def __init__(self):
         self.__capacity = 5
-        self.count = 0
+        self.__count = 0
         self.__table = [[] for _ in range(self.__capacity)]
+
 
     def hashCode(self, key):
         strKey = str(key)
         return int(hashlib.sha256(strKey.encode()).hexdigest(), 16) % self.__capacity
 
     def __len__(self):
-        return self.count
+        return self.__count
 
     def __setitem__(self, key, value):
         index = self.hashCode(key)
@@ -31,17 +32,20 @@ class HashTable:
                 element.value = value
                 return
         self.__table[index].append(new_element)
-        self.count += 1
+        self.__count += 1
 
     def __delitem__(self, key):
         index = self.hashCode(key)
         for element in self.__table[index]:
             if element.key == key:
                 self.__table[index].remove(element)
-                self.count -= 1
+                self.__count -= 1
                 return element.value
         
         raise KeyError(f'{key}')
+
+    
+
 
 phones = HashTable()
 phones['Steve'] = '9999-1111'
